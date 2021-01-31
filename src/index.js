ff61e8a78e52ddf0027fad61b5c65d237fed466a
sergJess <serg.fantasy@yandex.ru>
2021-01-31 16:13:10 +0300

    feat: add solution for task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (typeof str === 'string' && str.length % 2 === 0 && Array.isArray(bracketsConfig)) {
+        let length = str.length;
+        let stack = [];
+        let lastIndex = 0;
+        const pairBrackets = {
+            '(': ')',
+            '[': ']',
+            '{': '}',
+            '1': '2',
+            '3': '4',
+            '5': '6'
+        }
+        const unPairBrackets = {
+            '|': '|',
+            '7': '7',
+            '8': '8'
+        }
+
+        for (let i = 0; i < length; i++) {
+            if (pairBrackets[str[i]] || (unPairBrackets[str[i]] !== unPairBrackets[stack[stack.length - 1]])) {
+                stack.push(str[i]);
+                continue;
+            }
+            if (stack.length) {
+                lastIndex = stack[stack.length - 1];
+                if ((str[i] === pairBrackets[lastIndex] || str[i] === unPairBrackets[lastIndex])) stack.pop();
+
+            }
+        }
+        return (!stack.length);
+
+    }
+    return false;
 }
